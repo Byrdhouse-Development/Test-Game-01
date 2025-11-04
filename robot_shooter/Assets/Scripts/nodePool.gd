586c05f752321671ffce4e3b035ac62dd324e90a
Falkyn <falkyn.worm@gmail.com>
2025-11-04 12:47:06 -0700

    Add node pooling system and integrate into test scene
    Introduced nodePool.gd script to manage reusable Node2D instances, reducing instantiation overhead. Added a PlayerPool node to testScene.tscn and attached the nodePool script for object pooling functionality.

---
+++ robot_shooter/Assets/Scripts/nodePool.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var nodeScene : PackedScene
+var cachedNodes : Array(Node2D)
+
+func _createNew() -> Node2D :
+	var node = nodeScene.instantiate()
+	cachedNodes.append(node)
+	get_tree().get_root().add_child(node)
+	return node
+
+func spawn() -> Node2D:
+	for node in cachedNodes:
+		if node.visible == false:
+			node.visible = true
+			return node
+	return _createNew()
